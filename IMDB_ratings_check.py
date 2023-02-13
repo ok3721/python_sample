099169bdf120ce9d7f9f1d48e89aa5d9635c8c4f
ok3721 <76622058+ok3721@users.noreply.github.com>
2023-02-13 20:39:30 +0800

    Create IMDB_ratings_check.py

---
+++ IMDB_ratings_check.py
@@ -0,0 +1,23 @@
+#Special thanks to https://github.com/itsmehemant7/PyMovieDb/
+from PyMovieDb import IMDB
+import json
+import pandas as pd
+
+file = 'C:/Users/X1 Carbon/Documents/name.xlsx'
+xl = pd.ExcelFile(file)
+df1 = xl.parse('Sheet1')
+
+for index, row in df1.iterrows():
+    imdb = IMDB()
+    res = imdb.get_by_name(row["Name"])
+    result = json.loads(res)
+    status = list(result)[0]
+    if status == "status":
+        print(row["Name"] + "no exists")
+        pass
+    else:
+        type = result["type"]
+        rating = result["rating"]['ratingValue']
+        name = result["name"]
+        director = result["director"][0]["name"]
+        print("Name: " + str(name) + "/Type: "+ str(type)+ "/Director: "+ str(director) + "/Rating: " +str(rating))
